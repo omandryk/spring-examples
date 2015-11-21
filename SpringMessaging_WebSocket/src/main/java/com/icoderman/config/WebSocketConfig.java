0c3a3e39d6372abf0b88fe73c2fe42a3b623c343
Oleksandr Mandryk <icoderman@gmail.com>
2015-11-21 22:50:50 +0100

    Implemented Spring WebSocket example

---
+++ SpringMessaging_WebSocket/src/main/java/com/icoderman/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.icoderman.config;
+
+import com.icoderman.app.handler.MarcoHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(marcoHandler(), "/marco").withSockJS();
+    }
+
+    @Bean
+    public WebSocketHandler marcoHandler() {
+        return new MarcoHandler();
+    }
+
+}
